8b1dedf55df36b1c60aa3d07f792c4b4c7e5ac23
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-04-21 21:46:12 -0600

    Chakra ui theme and setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import {ChakraProvider} from '@chakra-ui/react';
+import {extendTheme} from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: '#3474',
+      900: 'blue',
+    },
+    primary: {
+      'orange': '#2023933'
+    }
+  },
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps}/>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
