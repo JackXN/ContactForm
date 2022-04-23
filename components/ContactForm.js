f2a07ed90d3d582901aa26d0a7ce2c89a20872b4
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-04-23 03:32:04 -0600

    dev done

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,47 +1,44 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-function ContactForm() {
-const [status, setStatus] = useState('Submit');
-
-const handleSubmit = async (e) => {
+const ContactForm = () => {
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setStatus('Sending....');
-    const {name,email,message} = e.target.elements;
-
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
     let details = {
-        name: name.value,
-        email: email.value,
-        message: message.value,
+      name: name.value,
+      email: email.value,
+      message: message.value,
     };
-    let response = await fetch('https://localhost:3000/contact', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json;charset=utf-8",
-        },
-        body:JSON.stringify(details)
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
     });
-    setStatus('Submit');
+    setStatus("Submit");
     let result = await response.json();
-    alert(result.status)
-}
+    alert(result.status);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" required />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" required />
+      </div>
+      <button type="submit">{status}</button>
+    </form>
+  );
+};
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor='name'>Name:</label>
-                <input type='text' required={true}/>
-            </div>
-            <div>
-                <label htmlFor='email'>Email: </label>
-                <input type='text' required={true}/>
-            </div>
-            <div>
-                <label htmlFor='text'>Message:</label>
-                <textarea required={true}/>
-            </div>
-            <button type='submit'>{status}</button>
-        </form>
-    )
-}
-
-export default ContactForm
+export default ContactForm;
