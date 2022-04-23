9b866d64dd2e4f918d0e5e7b8afd5852a030b927
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-04-23 02:55:45 -0600

    bump

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -8,14 +8,30 @@ const handleSubmit = async (e) => {
     setStatus('Sending....');
     const {name,email,message} = e.target.elements;
 
+    let details = {
+        name: name.value,
+        email: email.value,
+        message: message.value,
+    };
+    let response = await fetch('https://localhost:3000/contact', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json;charset=utf-8",
+        },
+        body:JSON.stringify(details)
+    });
+    setStatus('Submit');
+    let result = await response.json();
+    alert(result.status)
 }
 
-
-
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='name'>Name:</label>
+                <input type='text' required={true}/>
+            </div>
+        </form>
     )
 }
 
